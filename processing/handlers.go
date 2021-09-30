0169e3efb83e4fe21f1b4b62374e112c75845bb9
Sebastian Green-Husted <geoffcake@gmail.com>
2021-09-30 14:48:08 -0400

    formatting

--- processing/handlers.go
+++ processing/handlers.go
@@ -6,44 +6,43 @@ import (
 	"github.com/apex/log"
 
 	"jabberwocky/cluster"
-	"jabberwocky/transport"
 	"jabberwocky/storage"
+	"jabberwocky/transport"
 )
 
-
 func HandleStorage(ctx context.Context) {
-		log.Info("Starting storage loop")
-		for msg := range cluster.Router.GetStorageOutbound() {
-			switch msg.Type {
-			case "server":
-				err := storage.SaveServer(ctx, msg.Content.(storage.Server))
+	log.Info("Starting storage loop")
+	for msg := range cluster.Router.GetStorageOutbound() {
+		switch msg.Type {
+		case "server":
+			err := storage.SaveServer(ctx, msg.Content.(storage.Server))
+			if err != nil {
+				log.Error(err.Error())
+			}
+		case "agent":
+			switch msg.SubType {
+			case "connect":
+				servers, err := storage.ListLiveServers(ctx)
 				if err != nil {
 					log.Error(err.Error())
 				}
-			case "agent":
-				switch msg.SubType {
-				case "connect":
-					servers, err := storage.ListLiveServers(ctx)
-					if err != nil {
-						log.Error(err.Error())
-					}
 
-					srvList := transport.NewMessage("server", "list", servers)
-					cluster.Router.Send(msg.Content.(string), cluster.LOCAL_AGENT, srvList)
-				}
-			case "script":
-				switch msg.SubType {
-				case "create":
-					log.Info("Createing a script")
-				}
-			default:
-				log.WithFields(log.Fields{
-					"type":    msg.Type,
-					"subtype": msg.SubType,
-				}).Info("Unknown message type")
+				srvList := transport.NewMessage("server", "list", servers)
+				cluster.Router.Send(msg.Content.(string), cluster.LOCAL_AGENT, srvList)
 			}
+		case "script":
+			switch msg.SubType {
+			case "create":
+				log.Info("Createing a script")
+			}
+		default:
+			log.WithFields(log.Fields{
+				"type":    msg.Type,
+				"subtype": msg.SubType,
+			}).Info("Unknown message type")
 		}
-		log.Info("Leaving storage loop")
+	}
+	log.Info("Leaving storage loop")
 }
 
 func HandleOutput(ctx context.Context) {
